671035b5bdf533e5efd70347498b09d3b27f278f
MINTMOCA\mintw <hodori991004@gmail.com>
2021-12-27 16:24:19 +0900

    [ Counting Sort ] C code Upload

---
+++ Counting Sort/C_Cpp/Counting_Sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define max 30
+
+void counting_sort(int* arr, int size) {
+
+	int count[max];
+	// Create Count Value
+	for (int i = 0; i < max; i++) {
+		count[i] = 0;
+	}
+
+	// Count Element
+	for (int i = 0; i < size; i++) {
+		int val = arr[i];
+		count[val]++;
+	}
+
+	// Print Sorted Array
+	for (int i = 0; i < max; i++) {
+		for (int j = 0; j < count[i]; j++) {
+			printf("%d ", i);
+		}
+	}
+}
+
+int main() {
+
+	int arr[] = { 1, 3, 0, 4, 5, 6, 3, 2, 3 };
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+	counting_sort(arr, size);
+
+	return 0;
+}
